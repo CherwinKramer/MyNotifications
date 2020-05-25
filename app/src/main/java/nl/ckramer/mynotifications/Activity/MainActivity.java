ad82cc17e4943555e2a36f5fcc940cf2405ab028
Cherwin <99033887@mydavinci.nl>
2020-05-25 18:44:15 +0200

    App commit
    App initial commit

---
+++ app/src/main/java/nl/ckramer/mynotifications/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package nl.ckramer.mynotifications.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import nl.ckramer.mynotifications.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

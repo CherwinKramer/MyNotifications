3a3641a1ce0aa6a577dcdb84ab5ca1e817a79283
Cherwin <99033887@mydavinci.nl>
2020-05-28 21:20:05 +0200

    Added dropdown menu to notification creation

--- app/src/main/java/nl/ckramer/mynotifications/Fragment/NotificationCreateFragment.java
+++ app/src/main/java/nl/ckramer/mynotifications/Fragment/NotificationCreateFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import nl.ckramer.mynotifications.R;
 
@@ -18,8 +20,16 @@ public class NotificationCreateFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notification_create, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notification_create, container, false);
+
+        String[] reminders = new String[] {"15 Minutes", "1 Hour", "6 Hours", "1 Day"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.dropdown_item, reminders);
+
+        AutoCompleteTextView editTextFilledExposedDropdown = view.findViewById(R.id.notification_reminderSpinner);
+        editTextFilledExposedDropdown.setAdapter(adapter);
+
+        return view;
     }
 }
